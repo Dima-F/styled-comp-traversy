8380b68c9a035976c32782a4f97d950737044f57
Fiialo Dima <dafw@live.ru>
2022-10-20 21:41:42 +0300

    basic setup

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyledHeader, Nav, Logo } from './styles/Header.styled'
+import { Container } from './styles/Container.styled'
+import { Button } from './styles/Button.styled'
+
+export default function Header() {
+  return (
+    <StyledHeader>
+      <Container>
+        <Nav>
+          <Logo src='./images/logo.svg' alt='' />
+          <Button>Try it free</Button>
+        </Nav>
+      </Container>
+    </StyledHeader>
+  )
+}
