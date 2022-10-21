299e9adec6763a848015888f43c9d9e5607356b8
Fiialo Dima <dafw@live.ru>
2022-10-22 01:30:48 +0300

    complete code

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { Container } from '../src/components/styles/Container.styled'
 import Header from '../src/components/Header.js'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './components/styles/Global'
+import content from './content'
+import Card from './components/Card'
+import Footer from './components/Footer'
 
 const theme = {
   colors: {
     header: '#ebfbff',
     body: '#fff',
     footer: '#003333'
-  }
+  },
+  mobile: '768px'
 }
 
 function App() {
@@ -17,8 +21,11 @@ function App() {
       <GlobalStyles/>
       <Header />
       <Container>
-        <h1>Hello world</h1>
+        {content.map((item, index) => (
+          <Card key={index} item={item} />
+        ))}
       </Container>
+      <Footer/>
     </ThemeProvider>
   );
 }
